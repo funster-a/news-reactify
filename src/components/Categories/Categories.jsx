4467fe26b40adb74fa97eda6916f645044070078
Amir Akhanov <axanov_a@inbox.ru>
2024-09-10 19:50:49 +0500

    add hook fetch, refactoring, withSkeleton

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,8 +1,15 @@
+import { all } from 'axios'
 import styles from './categories.module.css'
 
 const Categories = ({categories, setSelectedCategory, selectedCategory}) => {
   return (
     <div className={styles.categories} >
+      <button 
+        className={!selectedCategory ? styles.active : styles.item} 
+        onClick={() => setSelectedCategory(null)}
+      >
+      All
+      </button>
       {categories.map((category) => {
         return (
           <button 
